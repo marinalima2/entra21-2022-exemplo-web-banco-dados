d730c5023ae39ad60ebf36fbe9a5c747dae4e9af
marina.lima <marinalimasc@gmail.com>
2022-08-03 22:08:13 -0300

    Criado o projeto clínica veterinária

---
+++ entra21Scharp.ClinicaVeterinariaAplicacao/entra21Scharp.ClinicaVeterinariaAplicacao/Controllers/RacaController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace entra21Scharp.ClinicaVeterinariaAplicacao.Controllers
+{
+    public class RacaController : Controller
+    {
+
+        [Route("/raca")]
+        [HttpGet]
+
+        public IActionResult ObterTodos()
+        {
+            return View("Index");
+        }
+
+        [Route("/raca/cadastrar")]
+        [HttpGet]
+       public IActionResult Cadastrar() 
+        {
+
+            return View();
+
+        }
+        
+        [Route("/raca/registrar")]
+        [HttpGet]
+        public IActionResult Registrar(
+        
+            [FromQuery] string nome,
+            [FromQuery]string especie )
+        {
+            return RedirectToAction("Index");
+        }
+
+        
+
+    }
+}
